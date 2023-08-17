995934f785c5acbe3a8f85d200de4d4e730182e0
JeffBigg <soriaarandajeferson@gmail.com>
2023-08-17 14:44:19 -0500

    agregar lineas

--- Calcugrupal.java
+++ Calcugrupal.java
@@ -5,6 +5,10 @@ public class Calcugrupal {
 
     public static void main(String[] args) {
         System.out.println("aqui empiesa todo");
+        System.out.println("aqui empiesa todo");
+        System.out.println("aqui empiesa todo");
+
+
     }
     
 }
